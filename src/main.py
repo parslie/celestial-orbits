44c080cf699342e6f901086fe096a8feed76a404
Viktor Holta <viktor.holta@outlook.com>
2021-12-12 20:53:30 +0100

    Added ability to quit with escape key

--- src/main.py
+++ src/main.py
@@ -87,7 +87,7 @@ def main():
         prev_time = curr_time
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 running = False
 
         bg = generate_bg(screen_size)
